e1eb8c6f23071c5be0b78caf8da1962125360dde
Artur Stopyra <astopyra@gmail.com>
2023-01-25 12:33:02 +0100

    19.3 DELETE method

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -16,5 +16,5 @@ public interface TaskRepository extends CrudRepository<Task, Long> {
     Optional<Task> findById(Long id);
 
     @Override
-    Task save(Task task);
+    Task save (Task task);
 }
