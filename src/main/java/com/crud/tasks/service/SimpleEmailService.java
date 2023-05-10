2404876c2c97f83afa44846a5ca30f79497c8f88
astopyra <114916280+astopyra@users.noreply.github.com>
2023-05-10 21:26:54 +0200

    32.3 create email template (daily task update)

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -26,7 +26,7 @@ public class SimpleEmailService {
     public void send(final Mail mail){
         log.info("Starting email preparation...");
         try {
-            javaMailSender.send(createMailMessage(mail));
+            javaMailSender.send(createMimeMessage(mail));
             log.info("Email has been sent.");
         } catch (MailException e) {
             log.error("Failed to process email sending: " + e.getMessage(), e);
